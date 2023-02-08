2faeb2468f78f78ac1c1b38ef8d7d0ed4b2e97cf
Andrei Neacșu <0x514@protonmail.com>
2023-02-08 02:48:35 +0200

    remove ts lint project

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
-    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
@@ -22,6 +21,5 @@ module.exports = {
     'no-undef': ['error'],
     'space-infix-ops': ['error'],
     '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/no-unnecessary-condition': ['error'],
   },
 };
